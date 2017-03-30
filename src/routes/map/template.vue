5578e9ea3a8ee55b869bbfd54a45a1b8ef3483e8
Swizz <gerodel.quentin@gmail.com>
2017-03-30 11:52:14 +0200

    Load pioupiou icon only when visible

--- src/routes/map/template.vue
+++ src/routes/map/template.vue
@@ -1,16 +1,20 @@
 <template lang="html">
   <section>
-    <v-map :zoom="zoom" :center="center">
+    <v-map :zoom="zoom" :center="center" @l-move="mapmove">
       <v-tilelayer
         :url="url"
         attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
       />
+      <template v-for="pioupiou in visiblePioupious">
+        <v-marker :lat-lng="{lat: pioupiou.location.latitude, lng: pioupiou.location.longitude}" :icon="icon"></v-marker>
+      </template>
     </v-map>
   </section>
 </template>
 
 <script lang="buble">
 import { Map as vMap, TileLayer as vTilelayer, Marker as vMarker } from 'vue2-leaflet'
+import L from 'leaflet'
 
 export default {
   name: 'map-view',
@@ -23,7 +27,39 @@ export default {
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       zoom: 5,
       center: [48.866667, 2.333333],
+      icon: L.icon({
+        iconUrl: 'static/pioupiou-logo-mini.svg',
+        iconRetinaUrl: 'static/pioupiou-logo-mini.svg',
+        iconSize: [80, 80],
+        iconAnchor: [40, 40]
+      }),
+      bounds: undefined,
       pioupious: []
+    }
+  },
+
+  methods: {
+    mapmove(evt) {
+      this.$set(this, 'bounds', evt.target.getBounds())
+      console.log(this.bounds)
+    }
+  },
+
+  computed: {
+    visiblePioupious() {
+      return this.pioupious.filter(pioupiou =>
+        pioupiou.location.latitude + pioupiou.location.longitude !== 0 &&
+        (
+          !this.bounds ||
+          (
+            this.bounds &&
+            pioupiou.location.latitude >= this.bounds.getSouthWest().lat &&
+            pioupiou.location.longitude >= this.bounds.getSouthWest().lng &&
+            pioupiou.location.latitude <= this.bounds.getNorthEast().lat &&
+            pioupiou.location.longitude <= this.bounds.getNorthEast().lng
+          )
+        )
+      )
     }
   },
 
@@ -37,8 +73,6 @@ export default {
 
     this.$http.get('live/all').then(({ body: response }) => {
       this.$set(this, 'pioupious', response.data)
-
-      console.log(this.pioupious)
     })
   }
 }
@@ -46,4 +80,8 @@ export default {
 
 <style lang="scss">
   @import "~leaflet/dist/leaflet.css";
+
+  .fake-icon-loader {
+    background-image: url("~static/img/pioupiou-logo-mini.svg");
+  }
 </style>
