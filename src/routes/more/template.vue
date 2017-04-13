37dcfae77bee9836d15e1255b405677bd8599e7c
Swizz <gerodel.quentin@gmail.com>
2017-04-13 15:16:30 +0200

    Add settings view

--- src/routes/more/template.vue
+++ src/routes/more/template.vue
@@ -1,19 +1,104 @@
 <template lang="html">
   <section>
-    <history-chart :data="data" style="height: 180px;"></history-chart>
+    <div class="columns">
+      <div class="column">
+        <div class="card">
+          <header class="card-header">
+            <div class="is-clearfix is-fullwidth">
+              <div class="is-pulled-left">
+                <strong>Units</strong> <br>
+                <small>Units available</small>
+              </div>
+              <div class="is-pulled-right">
+                <div class="field has-addons">
+                  <p class="control" v-for="(unit, key) in $convert.available">
+                    <a @click="changeUnit(key)"
+                      :class="['button is-small is-primary', $convert.current === key ? '' : 'is-outlined is-inverted']">
+                      <span>{{ unit }}</span>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+        <div class="card">
+          <header class="card-header">
+            <div class="is-clearfix is-fullwidth">
+              <div class="is-pulled-left">
+                <strong>Language</strong> <br>
+                <small>Choose your language</small>
+              </div>
+              <div class="is-pulled-right">
+                <div class="field has-addons">
+                  <p class="control" v-for="(language, key) in $language.available">
+                    <a @click="changeLanguage(key)"
+                      :class="['button is-small is-primary', $language.current === key ? '' : 'is-outlined is-inverted']">
+                      <span>{{ language }}</span>
+                    </a>
+                  </p>
+                  <p class="control">
+                    <a class="button is-small is-primary is-outlined is-inverted">
+                      <i class="fa fa-plus"></i>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+        <div class="card">
+          <header class="card-header">
+            <div class="is-clearfix is-fullwidth">
+              <div class="is-pulled-left">
+                <strong>Support</strong> <br>
+                <small>Need some help ?</small>
+              </div>
+              <div class="is-pulled-right">
+                <small>
+                  <a href="mailto:support@pioupiou.fr">support@pioupiou.fr</a>
+                </small>
+              </div>
+            </div>
+          </header>
+        </div>
+        <div class="card">
+          <header class="card-header">
+            <div class="is-clearfix is-fullwidth">
+              <div class="is-pulled-left">
+                <strong>Open Data</strong> <br>
+                <small>Data are free for everyone !</small>
+              </div>
+              <div class="is-pulled-right">
+                <small>
+                  <a href="http://developers.pioupiou.fr" target="_blank">http://developers.pioupiou.fr</a>
+                </small>
+              </div>
+            </div>
+          </header>
+        </div>
+        <div class="card">
+          <header class="card-header">
+            <div class="is-clearfix is-fullwidth">
+              <div class="is-pulled-left">
+                <img src="~static/img/pioupiou-logo.svg" alt="Pioupiou logo">
+              </div>
+              <div class="is-pulled-right">
+                <small>
+                  <strong>Version 0.4.0</strong> - beta
+                </small>
+              </div>
+            </div>
+          </header>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script lang="buble">
-import historyChart from '@/components/history-chart'
-
 export default {
   name: 'more-view',
-
-  components: { historyChart },
-
-  computed: {
-  },
 
   data() {
     return {
@@ -21,15 +106,78 @@ export default {
     }
   },
 
-  mounted() {
-    const start = new Date(new Date().getTime() - (3 * 3600 * 1000)).toISOString()
-    this.$http.get(`archive/614?start=${start}&stop=now`).then(({ body }) => {
-      this.data = body.data
-    })
+  methods: {
+    changeLanguage(key) {
+      this.$language.current = key
+    },
+    changeUnit(key) {
+      this.$convert.current = key
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  @import "~src/assets/vars";
 
+  .columns {
+    margin-left: 0;
+    margin-right: 0;
+    margin-top: 0;
+    margin-bottom: 0;
+
+    .column {
+      padding: 0;
+
+      &:last-child {
+        padding-bottom: 3rem;
+      }
+    }
+
+    &:last-child {
+      margin-bottom: initial;
+    }
+  }
+
+  .card {
+    box-shadow: initial;
+    border-top: 1px solid rgba(10, 10, 10, 0.1);
+
+    &:last-child {
+      border-bottom: 1px solid rgba(10, 10, 10, 0.1);
+    }
+
+    .card-header {
+      padding: 12px 12px 0px;
+      box-shadow: initial;
+
+      .title {
+        margin-bottom: 0;
+      }
+
+      .is-pulled-left {
+        max-width: 75%;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+      }
+
+      .is-pulled-right {
+        line-height: 1.9rem;
+      }
+
+      .field.has-addons {
+        margin-top: 5px;
+      }
+    }
+  }
+
+  small > a {
+    color: $primary;
+    text-decoration: underline;
+  }
+
+  img {
+    width: 7em;
+  }
 </style>
