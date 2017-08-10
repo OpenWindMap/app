ace7bb02bc8da90e14024ee2bf8694da5177a5d3
benoit <benoit.arguel@gmail.com>
2017-08-10 14:17:36 +0200

    style: new marker

--- src/components/wind-marker/WindMarker.js
+++ src/components/wind-marker/WindMarker.js
@@ -1,9 +1,10 @@
 import L from 'leaflet'
+import filters from '@/filters'
 
 const windIcon = L.divIcon({
-  iconSize: [26, 48],
+  iconSize: [25, 35], // [26, 48],
   popupAnchor: [1, -10],
-  iconAnchor: [13, 12]
+  iconAnchor: [12.5, 8.75] // [13, 12]
 })
 
 export const WindMarker = L.Marker.extend({
@@ -17,21 +18,30 @@ export const WindMarker = L.Marker.extend({
   _setPos(pos) {
     L.Marker.prototype._setPos.call(this, pos)
 
-    this._icon.style.background = 'url("./img/map-wind-icon.png")'
     this._icon.style.border = 'none'
+    this._icon.style.background = 'rgba(0, 0, 0, 0)'
 
-    let colorIndex = Math.floor(this.options.speed / 5)
-    colorIndex = (colorIndex > 15) ? 15 : colorIndex
-    const colorX = -colorIndex * 26
-
-    this._icon.style['background-position'] = colorX + 'px 0px'
+    console.log()
 
     const rotate = 'rotate(' + this.options.heading + 'deg)'
 
     this._icon.style[L.DomUtil.TRANSFORM] += ' ' + rotate
-    this._icon.style[L.DomUtil.TRANSFORM + '-origin'] = '13px 10px'
+    this._icon.style[L.DomUtil.TRANSFORM + '-origin'] = '12.5px 10px'
+    this._icon.style[L.DomUtil.TRANSFORM + '-origin'] = '12.5px 10px'
 
     this._icon.title = this.options.title
+
+    this._icon.innerHTML = `<svg width="25px" height="35px" style="background: rgba(0,0,0,0);">
+      <defs>
+        <filter id="f3" x="0" y="0" width="100%" height="100%">
+          <feOffset result="offOut" in="SourceAlpha" dx="0" dy="0" />
+          <feGaussianBlur result="blurOut" in="offOut" stdDeviation="1.5" />
+          <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+        </filter>
+      </defs>
+      <polygon points="22,9.75 12.5,28 3,9.75 12.5,14.75" style="filter:url(#f3)" fill="${filters.speedToColors(this.options.speed)}" />
+      <circle cx="12.5" cy="5" r="4.5" fill="#222"/>
+    </svg>`
 
     return this._icon.title
   },
