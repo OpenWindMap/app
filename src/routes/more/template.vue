46415c8c82a54cbef153c314fa68b177906cbf8d
Swizz <gerodel.quentin@gmail.com>
2017-11-27 12:17:42 +0100

    1.1.2

--- src/routes/more/template.vue
+++ src/routes/more/template.vue
@@ -270,7 +270,7 @@
               </div>
               <div class="is-pulled-right">
                 <small>
-                  <strong>Version 1.1.0</strong>
+                  <strong>Version 1.1.2</strong>
                 </small>
               </div>
             </div>
